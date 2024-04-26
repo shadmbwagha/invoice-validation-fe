c113cd3bd9a405629515cf29b18dde37c6734c2a
shad <105544140+shadmbwagha@users.noreply.github.com>
2024-04-26 06:56:46 -0700

    push

--- src/views/ExtractionForm.vue
+++ src/views/ExtractionForm.vue
@@ -148,31 +148,6 @@
                  
               </tr>
 
-              
-              <!-- <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" v-for="invoice in invoices" :key="invoice.id">
-                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    <router-link to="/devp">
-                        {{ invoice.filename }}
-                    </router-link>
-                      
-                  </th>
-                  <td class="px-6 py-4">
-                      {{ invoice.path }}
-                  </td>
-                  <td class="px-6 py-4">
-                      {{ invoice.created_at }}
-                  </td>
-                  <td class="px-6 py-4">
-                      {{ invoice.format }}
-                  </td>
-                  <td class="px-6 py-4 text-right">
-                    <svg class="w-6 h-6 text-blue-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
-                    </svg>
-
-
-                  </td>
-              </tr> -->
           </tbody>
       </table>
 
@@ -204,17 +179,7 @@
   
         try {
           axios.get('/invoices').then((data)=>{
-              this.invoices = data.data.invoices;
-              console.log(this.invoices);
-  
-              this.invoices.forEach(invoice => {
-              const filenameParts = invoice.filename.split('.');
-              if (filenameParts.length > 1) {
-                  invoice.format = filenameParts.pop(); // Get the last part as the format
-              } else {
-                  invoice.format = 'Unknown'; // If no extension found
-              }
-              });
+            this.$toast.error("failed to extract text");
           })
         } catch (error) {
           console.error('Error fetching invoices:', error.response);
